6f62c0466ef1a14a163d26d6317b76a6860ede14
Tom Jardine-McNamara <tom@itsravenous.com>
2017-09-27 23:55:00 +0100

    Import and use i3s swift library

--- Package.swift
+++ Package.swift
@@ -7,14 +7,13 @@ let package = Package(
     name: "MyApp2",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/itsravenous/SwiftMyLibrary", from: "1.0.1"),
-        .package(url: "https://github.com/mwsrp/i3s-swift-wrapper", from: "1.0.0"),
+        .package(url: "https://github.com/itsravenous/SwiftCppPackage", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MyApp2",
-            dependencies: ["MyLibrary", "Shark"]),
+            dependencies: ["i3s-swift"]),
     ]
 )
